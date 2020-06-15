b130556f887f0d3117457308d71bbc8875c16a86
Reggie Walters <reggiewalters@u.boisestate.edu>
2020-06-15 14:55:54 -0700

    Add files via upload

---
+++ CPI.m
@@ -0,0 +1,22 @@
+function [c_i, dt] = CPI(Precip, beta, time)
+% current precipitation index (CPI) calculation routine
+% as desribed in:
+% Smakhtin, V. Y., Masse, B. 2000. Continuous daily hydrograph simulation
+% using duration curves of a precipitation index. Hydrological Processes
+% 14: 1083-1100.
+% r. walters, sfpuc, june 2016
+%
+% INPUT:
+%       Precip: instantaneous precipitation time series vector
+%       beta:   recession coefficient
+%       time:   time array corresponding to precipitation input series
+%
+
+% initialize container (all zeroes) for function output variable, ci
+c_i = 0 .* ones(length(Precip),1);          
+
+for t = 2:length(c_i)
+    c_i(t) = Precip(t-1) + beta * c_i(t-1);
+end
+
+dt = time;
