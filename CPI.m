865ac286790a7ceec1510d330f49dde0b43fe7f5
Reggie Walters <reggiewalters@u.boisestate.edu>
2020-09-09 10:56:50 -0700

    Update CPI.m

--- CPI.m
+++ CPI.m
@@ -4,7 +4,9 @@ function [c_i, dt] = CPI(Precip, beta, time)
 % Smakhtin, V. Y., Masse, B. 2000. Continuous daily hydrograph simulation
 % using duration curves of a precipitation index. Hydrological Processes
 % 14: 1083-1100.
-% r. walters, sfpuc, june 2016
+% r. walters, sfpuc, june 2020
+% EDITED sept 2020 to fix an index error
+%   --> Precip(t-1) should be Precip(t) [catchment precip for current day]
 %
 % INPUT:
 %       Precip: instantaneous precipitation time series vector
@@ -16,7 +18,7 @@ function [c_i, dt] = CPI(Precip, beta, time)
 c_i = 0 .* ones(length(Precip),1);          
 
 for t = 2:length(c_i)
-    c_i(t) = Precip(t-1) + beta * c_i(t-1);
+    c_i(t) = Precip(t) + beta * c_i(t-1);
 end
 
 dt = time;
